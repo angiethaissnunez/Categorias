5b9f27a042ba5c65816467aed5c07294f28dc25a
angie nunez <angiethaiss52@gmail.com>
2022-03-23 15:58:33 -0600

    Initial commit Probando

---
+++ app/src/main/java/com/example/categorias/ActivityCGB.java
@@ -0,0 +1,14 @@
+package com.example.categorias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCGB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cgb);
+    }
+}
